298c2abec7c56ca960289badd0201e3ff05ee182
David Boller <david@virtual-graphics.ch>
2023-09-13 09:44:56 +0200

    Fixed bugs navbar

--- components/modules/navbar/NavBar.tsx
+++ components/modules/navbar/NavBar.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import Image from 'next/image'
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import Link from 'next/link'
 
 
 function NavBar() {
  const [isOpen, setIsOpen] = useState(false);
 
  return (
-   <nav className="flex items-center justify-between flex-wrap p-4 border-b border-blue-200">
+   <nav className="flex items-center justify-between flex-wrap p-4 border-b border-blue-200 bg-black">
      <div className="flex items-center flex-shrink mr-6 lg:mr-72">
 
      <Image
@@ -21,7 +22,7 @@ function NavBar() {
      <div className="block lg:hidden">
        <button
          onClick={() => setIsOpen(!isOpen)}
-         className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+         className="flex items-center px-3 py-2 rounded text-white hover:text-black-400"
        >
          <svg
            className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
@@ -44,18 +45,21 @@ function NavBar() {
      >
        <div className="text-md font-Proza lg:flex-grow">
 
-         <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100 ">
+         <Link href="/" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100 ">
            HOMEWORLD
-         </a>
-         <a href="games" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100">
+         </Link>
+         <Link href="games" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100">
            GAMES
-         </a>
-         <a href="nftmint" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100">
+         </Link>
+         <Link href="nftmint" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100">
            NFTs
-         </a>
-         <a href="token" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100">
+         </Link>
+         <Link href="token" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100">
            TOKENS
-         </a>
+         </Link>
+         <Link href="/" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-12  text-white hover:text-yellow-100">
+           MARKETPLACE
+         </Link>
        </div>
        <div className="py-2 mr-12">
        <ConnectWallet />
