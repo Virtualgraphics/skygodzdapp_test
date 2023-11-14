b8f2d3760a5f6fdd1916a12af844b2d537c22590
David Boller <david@virtual-graphics.ch>
2023-11-14 20:05:06 +0100

    Landing page finished

--- components/templates/home/Home.tsx
+++ components/templates/home/Home.tsx
@@ -5,6 +5,7 @@ import { Hero } from '../hero';
 import { Intro } from '../intro';
 import Image from 'next/image'
 import Link from 'next/link'
+import { Credits } from '../credits';
 
 
   
@@ -18,6 +19,19 @@ import Link from 'next/link'
    <FrontSlider/>
 
 <div className="px-6">
+
+<div className="border-2 border-blue-100/50 bg-gradient-to-r from-black to-indigo-800 py-8 px-4 mt-8 w-1/2 justify-center m-auto grid grid-rows-2 gap-4 rounded-3xl">
+
+<div>
+<h1 className="text-blue-100 text-lg font-Proza text-center font-semibold">YOUR STARDAPP Dashboard</h1>
+</div>
+<div>
+<h1 className="text-white text-lg font-Proza text-center">Connect a wallet to see your assets</h1>
+</div>
+
+</div>
+
+
    <Hero />
 
 <div className="grid grid-rows gap-4 ">
@@ -34,7 +48,7 @@ import Link from 'next/link'
 
 </div>
 
-<div className="  sm:w-1/2  md:w.1/2 lg:w-3/4 justify-center m-auto ">
+<div className="  w-3/4 justify-center m-auto ">
 <h1 className="text-center text-blue-100 text-xl font-Proza">
 The Stargazer Advent Calendar will reveal one exclusive alien NFT, every day from December 1 to December 25, 2023 to be used as assets in the DeFi and RPG Game.</h1>
 
@@ -53,6 +67,10 @@ The Stargazer Advent Calendar will reveal one exclusive alien NFT, every day fro
 <hr className="w-2/4  border-yellow-200 justify-center m-auto flex " />
 
    <Intro />
+
+   <hr className="w-2/4  border-yellow-200 justify-center m-auto flex " />
+
+   <Credits/>
 
    </div>
 
